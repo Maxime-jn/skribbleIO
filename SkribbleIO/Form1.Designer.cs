86d0b9754e9d9044749198d7267f9e4affc1c692
sspirig <santiago.sprg@eduge.ch>
2025-05-07 15:29:43 +0200

    Added Canvas with draw function

--- SkribbleIO/Form1.Designer.cs
+++ SkribbleIO/Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace SkribbleIO
+﻿using System.Windows.Forms;
+
+namespace SkribbleIO
 {
     partial class Form1
     {
@@ -28,12 +30,84 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            pbxCanvas = new PictureBox();
+            btnPen = new Button();
+            btnEraser = new Button();
+            trbWidth = new TrackBar();
+            ((System.ComponentModel.ISupportInitialize)pbxCanvas).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trbWidth).BeginInit();
+            SuspendLayout();
+            // 
+            // pbxCanvas
+            // 
+            pbxCanvas.BackColor = Color.White;
+            pbxCanvas.Image = (Image)resources.GetObject("pbxCanvas.Image");
+            pbxCanvas.Location = new Point(174, 59);
+            pbxCanvas.Name = "pbxCanvas";
+            pbxCanvas.Size = new Size(880, 545);
+            pbxCanvas.TabIndex = 0;
+            pbxCanvas.TabStop = false;
+            pbxCanvas.MouseDown += pbxCanvas_MouseDown;
+            pbxCanvas.MouseMove += pbxCanvas_MouseMove;
+            pbxCanvas.MouseUp += pbxCanvas_MouseUp;
+            // 
+            // btnPen
+            // 
+            btnPen.Location = new Point(1080, 105);
+            btnPen.Name = "btnPen";
+            btnPen.Size = new Size(91, 65);
+            btnPen.TabIndex = 1;
+            btnPen.Text = "crayon";
+            btnPen.UseVisualStyleBackColor = true;
+            btnPen.Click += btnPen_Click;
+            // 
+            // btnEraser
+            // 
+            btnEraser.Location = new Point(1080, 197);
+            btnEraser.Name = "btnEraser";
+            btnEraser.Size = new Size(91, 65);
+            btnEraser.TabIndex = 2;
+            btnEraser.Text = "gomme";
+            btnEraser.UseVisualStyleBackColor = true;
+            btnEraser.Click += btnEraser_Click;
+            // 
+            // trbWidth
+            // 
+            trbWidth.LargeChange = 10;
+            trbWidth.Location = new Point(708, 623);
+            trbWidth.Maximum = 150;
+            trbWidth.Minimum = 5;
+            trbWidth.Name = "trbWidth";
+            trbWidth.Size = new Size(346, 45);
+            trbWidth.SmallChange = 5;
+            trbWidth.TabIndex = 4;
+            trbWidth.Value = 5;
+            trbWidth.Scroll += trbWidth_Scroll;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Desktop;
+            ClientSize = new Size(1244, 680);
+            Controls.Add(trbWidth);
+            Controls.Add(btnEraser);
+            Controls.Add(btnPen);
+            Controls.Add(pbxCanvas);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pbxCanvas).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trbWidth).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private PictureBox pbxCanvas;
+        private Button btnPen;
+        private Button btnEraser;
+        private TrackBar trbWidth;
     }
 }
--- SkribbleIO/Form1.cs
+++ SkribbleIO/Form1.cs
@@ -1,3 +1,5 @@
+using System.Windows.Forms;
+
 namespace SkribbleIO
 {
     public partial class Form1 : Form
@@ -5,6 +7,68 @@ namespace SkribbleIO
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private bool isDrawing = false;
+        private Point lastPoint = Point.Empty;
+        private Color selectedColor = Color.Black;
+        private int selectedWidth = 5;
+        private void pbxCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDrawing && pbxCanvas.Image != null)
+            {
+                Bitmap bmp = (Bitmap)pbxCanvas.Image;
+                using (Graphics g = Graphics.FromImage(bmp))
+                {
+                    using (Pen p = new Pen(selectedColor, selectedWidth))
+                    using (Brush b = new SolidBrush(selectedColor))
+                    {
+                        g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                        g.DrawLine(p, lastPoint, e.Location);
+
+                        // Remplit les trous entre les points en dessinant un petit cercle à l'arrivée
+                        int radius = selectedWidth / 2; // rayon du cercle (à ajuster selon l'épaisseur)
+                        g.FillEllipse(b, e.X - radius, e.Y - radius, radius * 2, radius * 2);
+                    }
+                }
+
+                pbxCanvas.Invalidate();
+                lastPoint = e.Location;
+            }
+        }
+        private void pbxCanvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            // Handle mouse down event
+            if (e.Button == MouseButtons.Left)
+            {
+                isDrawing = true;
+                lastPoint = e.Location;
+            }
+        }
+
+        private void pbxCanvas_MouseUp(object sender, MouseEventArgs e)
+        {
+            // Handle mouse up event
+            if (e.Button == MouseButtons.Left)
+            {
+                isDrawing = false;
+            }
+        }
+
+        private void btnPen_Click(object sender, EventArgs e)
+        {
+            selectedColor = Color.Black;
+        }
+
+        private void btnEraser_Click(object sender, EventArgs e)
+        {
+            selectedColor = Color.White; // Utilise la couleur de fond pour effacer
+
+        }
+
+        private void trbWidth_Scroll(object sender, EventArgs e)
+        {
+            selectedWidth = trbWidth.Value;
         }
     }
 }
